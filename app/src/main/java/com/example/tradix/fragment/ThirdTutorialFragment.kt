9279eeaa3f87cc8ea5c83d7518725eea5830bf8e
LeHongDuc274 <LeHongDuc274@gmail.com>
2021-08-24 18:36:18 +0700

    signin-up layout

--- app/src/main/java/com/example/tradix/fragment/ThirdTutorialFragment.kt
+++ app/src/main/java/com/example/tradix/fragment/ThirdTutorialFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.viewpager2.widget.ViewPager2
 import com.example.tradix.R
 
 class ThirdTutorialFragment : Fragment() {
@@ -14,7 +17,16 @@ class ThirdTutorialFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_tutorial, container, false)
+        val view = inflater.inflate(R.layout.fragment_third_tutorial, container, false)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+        view.findViewById<ImageButton>(R.id.btn_back_3).setOnClickListener {
+            viewPager?.currentItem?.minus(1)
+        }
+        view.findViewById<Button>(R.id.btn_next_3).setOnClickListener {
+            //navigation login
+
+        }
+        return view
     }
 
 }
